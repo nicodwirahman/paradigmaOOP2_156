ff75cfae4f940e8d09f4ff026bf464967a4a208e
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-29 18:05:19 +0700

    class remotelampu

---
+++ progaram enkpapsulasi/progaram enkpapsulasi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+	string saklarNo[10];
+public:
+	void setsaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getsaklarNo(int i) {
+		return saklarNo[i];
+	}
+};
