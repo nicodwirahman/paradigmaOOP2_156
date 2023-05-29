d2f2706c5914128e2725b9e80a41815fe651e724
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-29 18:19:46 +0700

    class abstraksiklas

---
+++ progaram abstraksi/progaram abstraksi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+	//method untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y =" << y << endl;
+	}
+};
