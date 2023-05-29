30a77fbf4f32092294fdec12a6c149409c7a9303
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-29 15:07:49 +0700

    buat pelajar

--- paradigmaOOP2_156/paradigmaOOP2_156.cpp
+++ paradigmaOOP2_156/paradigmaOOP2_156.cpp
@@ -38,4 +38,10 @@ public:
 		sekolah(pSekolah) {
 		cout << "pelajar dibuat\n <<endl";
 	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return"hallo, nama saya" + nama + "dengan jenis kelamin" + jeniskelamin
+	}
 };
