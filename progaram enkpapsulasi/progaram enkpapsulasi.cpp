e2f4889100d3b8fae41f68ad54d8f79295425672
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-29 18:14:04 +0700

    buat set saklar lampu

--- progaram enkpapsulasi/progaram enkpapsulasi.cpp
+++ progaram enkpapsulasi/progaram enkpapsulasi.cpp
@@ -12,3 +12,19 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setsaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setsaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getsaklarNo(0) << endl;
+	cout << lampuRumah.getsaklarNo(1) << endl;
+	cout << lampuRumah.getsaklarNo(2) << endl;
+	cout << lampuRumah.getsaklarNo(3) << endl;
+
+	return 0;
+}
